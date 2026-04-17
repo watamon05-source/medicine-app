d7640510da728a7866f162a7d733fcf8d09c731c
rina <watamon0406@outlook.jp>
2026-04-17 23:07:23 +0900

    PWA対応

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "服薬管理アプリ",
+        short_name: "服薬",
+        theme_color: "#3B82F6",
+        background_color: "#ffffff",
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+      },
+    }),
+  ],
+});
